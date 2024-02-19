492dd6d905229403b3fd770c540c3f3a4545645f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-19 18:59:34 +0900

    Add setup function

--- lua/human-rights/init.lua
+++ lua/human-rights/init.lua
@@ -3,11 +3,34 @@ local memory = require("human-rights.memory")
 local parallelism = require("human-rights.parallelism")
 local disk = require("human-rights.disk")
 
+---@type table
+local DEFAULT_HUMAN_RIGHTS = {
+    cpu = {
+        "Ryzen 9 7950X",
+        "i9-14900K",
+    },
+    memory = {
+        -- 31GB
+        linux = 33285996544,
+        -- 31GB
+        windows = 33285996544,
+    },
+    parallelism = 24,
+    disk = "SSD",
+}
+
 local M = {}
+M.human_rights = DEFAULT_HUMAN_RIGHTS
 
 M.show_cpu = cpu.show_cpu
 M.show_memory = memory.show_memory
 M.show_parallelism = parallelism.show_parallelism
 M.show_disk = disk.show_disk
+
+function M.setup(opts)
+    if opts and opts.human_rights then
+        M.human_rights = opts.human_rights
+    end
+end
 
 return M
